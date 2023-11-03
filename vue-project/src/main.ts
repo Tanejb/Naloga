60872566a2780dbc9708afea1a114a40203f9266
tanej.buhin <tanej.buhin@student.um.si>
2023-11-03 09:49:16 +0100

    dodal primevue toast in majhni popravki

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -2,9 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
+import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
+import 'primevue/resources/themes/lara-light-teal/theme.css'
+
+
+
 
 const app = createApp(App)
+app.use(PrimeVue);
+app.use(router);
+app.use(ToastService);
 
-app.use(router)
-
-app.mount('#app')
+app.mount('#app');
